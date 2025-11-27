7e00e57107384574ca848f68a0146a1d3f2c16c7
superisuer <superisuer@proton.me>
2025-11-27 17:07:38 +0000

    фикс багов

--- main.py
+++ main.py
@@ -21,20 +21,19 @@ load_dotenv()
 ollama_client = AsyncClient(host=OLLAMA_HOST)
 
 API_TOKEN = os.getenv("API_TOKEN")
-if not API_TOKEN:
-    error("API_TOKEN is not set")
-    sys.exit(1)
-
-API_ID = os.getenv("API_ID")
-if not API_ID:
-    error("API_ID is not set")
-    sys.exit(1)
-
 API_HASH = os.getenv("API_HASH")
-if not API_HASH:
-    error("API_HASH is not set")
+API_ID = os.getenv("API_ID")
+if not API_TOKEN:
+    error("API_TOKEN не установлен. Создайте бота в @BotFather и установите токен бота.")
     sys.exit(1)
 
+if not API_ID:
+    error("API_ID не установлен. Посети https://my.telegram.org/, чтобы получить его.")
+    sys.exit(1)
+    
+if not API_HASH:
+    error("API_HASH не установлен. Посети https://my.telegram.org/, чтобы получить его.")
+    sys.exit(1)
 
 app = Client(
     "uzbekgpt",
@@ -47,7 +46,11 @@ user_contexts = {}
 
 @app.on_message(filters.command("start") & filters.private)
 async def start_handler(client: Client, message: Message):
-    user_id = message.from_user.id
+    if message.sender_chat:
+        user_id = message.sender_chat.id
+    else:
+        user_id = message.from_user.id
+    
     user_contexts[user_id] = []
 
     keyboard = InlineKeyboardMarkup(
@@ -60,11 +63,11 @@ async def start_handler(client: Client, message: Message):
             reply_markup=keyboard
         )
     except Forbidden as e:
-        print(f"Error in sending message: {e}")
+        error(e)
         user_contexts[user_id] = []
     except Exception as e:
-        await message.reply("⚠️ узбекгпт не смог ответить вам. мы сбросили ваш контекст.")
-        print(f"Error in sending message: {e}")
+        await message.reply("⚠️ узбекгпт не смог ответить вам. ")
+        error(e)
         user_contexts[user_id] = []
 
 @app.on_callback_query(filters.regex("^start$"))
@@ -77,58 +80,38 @@ async def start_callback(client, callback_query):
 
 @app.on_message(filters.command("clear") & filters.incoming)
 async def clear_handler(client: Client, message: Message):
-    user_id = message.from_user.id
+    if message.sender_chat:
+        user_id = message.sender_chat.id
+    else:
+        user_id = message.from_user.id    
     user_contexts[user_id] = []
     try:
         await message.reply("контекст очичен✅")
     except Forbidden as e:
-        error(f"Error in sending message: {e}")
+        error(e)
         user_contexts[user_id] = []
     except Exception as e:
-        await message.reply("⚠️ узбекгпт не смог ответить вам. мы сбросили ваш контекст.")
-        error(f"Error in sending message: {e}")
+        await message.reply("⚠️ узбекгпт не смог ответить вам. ")
+        error(e)
         user_contexts[user_id] = []
 
 @app.on_message(filters.text)
 async def text_handler(client, message):
-    user_id = message.from_user.id
+    if message.sender_chat:
+        user_id = message.sender_chat.id
+    else:
+        user_id = message.from_user.id
     if message.chat.type in [ChatType.GROUP, ChatType.SUPERGROUP]:        
-        text_lower = (message.text or "").lower()
+        me = await client.get_me()
         is_reply_to_bot = (message.reply_to_message and message.reply_to_message.from_user and message.reply_to_message.from_user.is_bot)
-        
-        mentions_bot = any(e.type == "mention" for e in (message.entities or []))
-        has_uzbek = "узбек" in text_lower
+        mentions_bot = me.username in message.text
+        has_uzbek = "узбек" in message.text.lower()
         # print(is_reply_to_bot, mentions_bot, has_uzbek)
         if not (is_reply_to_bot or mentions_bot or has_uzbek):
             return
 
-    prompt = ""
+    prompt = message.text
 
-    if message.text:
-        prompt = message.text
-    elif message.caption:
-        prompt = message.caption
-
-    if message.photo:
-        photo = message.photo[-1]
-    
-        path = f"{message.id}.jpg"
-        await client.download_media(photo.file_id, file_name=path)
-    
-        img = Image.open(path)
-        img = img.convert("L")
-        img = img.point(lambda x: 0 if x < 140 else 255, "1")
-    
-        config = r"--oem 3 --psm 6"
-        text_img = pytesseract.image_to_string(img, lang="rus+eng", config=config)
-    
-        os.remove(path)
-    
-        if prompt and text_img:
-            prompt = f"{prompt}\n<фото {path}>{text_img}</image>"
-        elif text_img:
-            prompt = f"<фото {path}>{text_img}/>"
-    
     if message.reply_to_message and message.reply_to_message.text:
         replied_text = message.reply_to_message.text
         prompt = f"<цитата>{replied_text}</цитата>{prompt}"
@@ -147,12 +130,12 @@ async def text_handler(client, message):
             action=ChatAction.TYPING
         )
     except Forbidden as e:
-        error(f"Error in sending chat action: {e}")
+        error(e)
         user_contexts[user_id] = []
         return
     except Exception as e:
         await message.reply("⚠️ узбекгпт не смог ответить вам. мы сбросили ваш контекст.")
-        error(f"Error in sending chat action: {e}")
+        error(e)
         user_contexts[user_id] = []
         return
 
@@ -171,14 +154,14 @@ async def text_handler(client, message):
 
     except asyncio.TimeoutError:
         task.cancel()
-        warn("The AI was unable to respond for more than 50 seconds")
-        await message.reply("⚠️отказ! наш узбек заснул на клавиатуре и не отвечал вам более 50 сек! чтобы очистить контекст и сменить узбека напиши /clear ✅")
+        warn("ЛЛМка не смогла ответить больше 50 секунд!!1!1")
+        await message.reply("⚠️к сожалению узбекгпт не придумал ответ за 50 секунд. отправьте сообщение ещё раз или очистите контекст командой /clear")
         return
 
     except Exception as e:
         task.cancel()
-        error(f"Ollama Error: {e}")
-        await message.reply("⚠️отказ! произошла ошибка при выполнении! контекст очищен ✅")
+        error(e)
+        await message.reply("⚠️отказ! произошла ошибка при выполнении! контекст очищен")
         user_contexts[user_id] = []
         return
 
@@ -191,11 +174,11 @@ async def text_handler(client, message):
     try:
         await message.reply(text)
     except Forbidden as e:
-        error(f"Error in sending message: {e}")
+        error(e)
         user_contexts[user_id] = []
     except Exception as e:
         await message.reply("⚠️ узбекгпт не смог ответить вам. мы сбросили ваш контекст.")
-        error(f"Error in sending message: {e}")
+        error(e)
         user_contexts[user_id] = []
 
 app.run()
